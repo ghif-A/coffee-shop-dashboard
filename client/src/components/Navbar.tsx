bdd1fea1bc813ff082f35b0630621bdf6b1110a8
ghif-A <ghiffariahmadijaya@gmail.com>
2024-04-29 09:51:04 +0700

    add navbar

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/coffee.png';
+import { useTheme } from '../contexts/ThemeContext'; // Import useTheme hook
+
+const Navbar = () => {
+  const { theme, toggleTheme } = useTheme(); // Destructure theme and toggleTheme from the context
+
+  return (
+    <nav className="navbar navbar-expand-lg fixed-top" style={{ backgroundColor: '#be9b7b' }}>
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/" style={{ color: '#4b3832' }}>
+          <img src={logo} alt="Analytics Dashboard Logo" height="30" className="d-inline-block align-top" />
+          {' '}Analytics Dashboard
+        </NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/" style={{ color: '#4b3832' }}>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/tables" style={{ color: '#4b3832' }}>
+                Tables
+              </NavLink>
+            </li>
+            {/* Theme toggle list item */}
+            <li className="nav-item">
+              <span className="nav-link" style={{ cursor: 'pointer', color: '#4b3832' }} onClick={toggleTheme}>
+                {theme === 'light' ? 'Dark Mode : OFF' : 'Dark Mode : ON'}
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
