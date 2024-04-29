dc89df586d81867a15fe641e0c4d0498cafbbed3
ghif-A <ghiffariahmadijaya@gmail.com>
2024-04-29 09:51:58 +0700

    remove theme toggle button and h1 title

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -4,14 +4,11 @@ import LineChart from "../components/LineChart";
 // import DiskUsageChart from "../components/DiskUsageChart";
 import DiskUsageTreeMap from "../components/DiskUsageTreeMap";
 import { useTheme } from "../contexts/ThemeContext";
-import ThemeToggle from "../components/ThemeToggle";
 
 const Home: React.FC = () => {
   const { theme } = useTheme();
   return (
     <div className={theme}>
-      <ThemeToggle />
-      <h1>Analytics Dashboard</h1>
       <BarChart />
       <LineChart />
       {/* <DiskUsageChart /> */}
--- client/src/views/Tables.tsx
+++ client/src/views/Tables.tsx
@@ -1,13 +1,10 @@
 import Table from "../components/Table";
 import { useTheme } from "../contexts/ThemeContext";
-import ThemeToggle from "../components/ThemeToggle";
 
 const Tables = () => {
   const { theme } = useTheme();
   return (
     <div className={theme}>
-      <ThemeToggle />
-      <h1>Tables</h1>
       <Table />
     </div>
   );
