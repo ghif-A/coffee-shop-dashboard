a7f85f923238a4ae100658a63fc57abfb08bacce
ghif-A <ghiffariahmadijaya@gmail.com>
2024-04-30 07:47:02 +0700

    export API endpoint

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { API_ENDPOINT } from './config.local';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/products': 'http://localhost:9000',
-      '/dailysales': 'http://localhost:9000',
-      '/dailyspoilage': 'http://localhost:9000',
-      '/productcategorysales': 'http://localhost:9000',
+      '/products': API_ENDPOINT,
+      '/dailysales': API_ENDPOINT,
+      '/dailyspoilage': API_ENDPOINT,
+      '/productcategorysales': API_ENDPOINT,
     }
   }
 })
