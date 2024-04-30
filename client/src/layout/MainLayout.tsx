87c44349f45b111fe55a805e855fc30f3d4fca68
ghif-A <ghiffariahmadijaya@gmail.com>
2024-04-30 21:25:58 +0700

    remove comment

--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import './MainLayout.css'; // Import the CSS file
+import './MainLayout.css';
 
 const MainLayout: React.FC = () => {
   return (
