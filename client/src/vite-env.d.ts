3d4ac174c6ffe02d49e5a46162fbce6dd8c5f7b1
ghif-A <ghiffariahmadijaya@gmail.com>
2024-04-30 09:10:44 +0700

    configure the api endpoint

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,10 @@
+import 'dotenv/config'
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_ENDPOINT: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { API_ENDPOINT } from './config.local';
+
+const API_ENDPOINT = 'https://coffee-shop-dashboard-backend.onrender.com'
 
 // https://vitejs.dev/config/
 export default defineConfig({
