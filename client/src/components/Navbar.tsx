5ddd16fd069277873ed544151c76bf4ea3dc711d
ghif-A <ghiffariahmadijaya@gmail.com>
2024-04-30 13:38:13 +0700

    switched the dark/light icons

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
             </li>
             <li className="nav-item">
               <span className="nav-link" style={{ cursor: 'pointer', color: '#4b3832' }} onClick={toggleTheme}>
-                {theme === 'light' ? '⚫' : '⚪'}
+                {theme === 'light' ? '⚪' : '⚫'}
               </span>
             </li>
           </ul>
